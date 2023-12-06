bbfb0867234e03c58cf45281fda07ac45969ecbf
Diogo Vaz <diogo.vaz@tecnico.ulisboa.pt>
2023-12-06 13:20:13 +0000

    #36 links add and publisher style updated

--- main.js
+++ main.js
@@ -17,39 +17,47 @@ publications_more_button.addEventListener("click", function() {
 addPaper("Synthesis of Fault-Tolerant Reliable Broadcast Algorithms with Reinforcement Learning",
         "<u>Diogo Vaz</u>, David R. Matos, Miguel L. Pardal and Miguel Correia",
         "IEEE Access Journal", "June 2023",
-        true)
+        true,
+        "https://www.researchgate.net/publication/371705768_Synthesis_of_Fault-Tolerant_Reliable_Broadcast_Algorithms_with_Reinforcement_Learning")
 
 addPaper("Automatic Generation of Distributed Algorithms with Generative AI",
         "<u>Diogo Vaz</u>, David R. Matos, Miguel L. Pardal and Miguel Correia",
         "The 53rd Annual IEEE/IFIP International Conference on Dependable Systems and Networks (Disrupt Track)", "June 2023",
-        true)
+        true,
+        "https://www.researchgate.net/publication/371760975_Automatic_Generation_of_Distributed_Algorithms_with_Generative_AI")
 
-addPaper("Aprender a gerar algoritmos",
+/*addPaper("Aprender a gerar algoritmos",
         "<u>Diogo Vaz</u>",
         "Público (Portuguese Journal)", "May 2023",
         false)
+*/
 
 addPaper("Learning to generate Reliable Broadcast Algorithms",
         "<u>Diogo Vaz</u>, David R. Matos, Miguel L. Pardal and Miguel Correia",
         "arXiv Preprint", "July 2022",
-        false)
+        false,
+        "https://www.researchgate.net/publication/362410565_Learning_to_generate_Reliable_Broadcast_Algorithms")
 
 addPaper("MIRES: Intrusion Recovery for Applications based on Backend-as-a-Service",
         "<u>Diogo Vaz</u>, David R. Matos, Miguel L. Pardal and Miguel Correia",
         "IEEE Transactions on Cloud Computing Journal", "May 2022",
-        true)
+        true,
+        "https://www.researchgate.net/publication/360965942_MIRES_Intrusion_Recovery_for_Applications_based_on_Backend-as-a-Service")
 
 addPaper("MIRES: Recovering Mobile Applications based on Backend-as-a-Service from Cyber Attacks",
         "<u>Diogo Vaz</u>, David R. Matos, Miguel L. Pardal and Miguel Correia",
         "7th EAI International Conference on Mobile and Ubiquitous Systems: Computing, Networking and Services (MobiQuitous)", "December 2020",
-        false)
+        false,
+        "https://www.researchgate.net/publication/353801489_MIRES_Recovering_Mobile_Applications_based_on_Backend-as-a-Service_from_Cyber_Attacks")
 
 addPaper("MIRES: Recovering Mobile Applications based on Backend-as-a-Service from Cyber Attacks",
         "<u>Diogo Vaz</u>",
-        "Master Thesis", "November 2020",false)
+        "Master Thesis", "November 2020",
+        false,
+        "https://www.researchgate.net/publication/367268385_MIRES_Recovering_Mobile_Applications_based_on_Backend-as-a-Service_from_Cyber_Attacks")
 
 /* ADD PAPER TO LIST */
-function addPaper(title,authors,publisher,date,selected_paper){
+function addPaper(title,authors,publisher,date,selected_paper,link){
     var element='\
     <div class="d-flex">\
         <!--div>\
@@ -58,11 +66,13 @@ function addPaper(title,authors,publisher,date,selected_paper){
         <div class="container w-75">\
             <p class="paper-title"><b>&#9679 '+title+'</b></p>\
             <p class="paper-authors">'+authors+'</p>\
-            <p class="paper-publisher">'+publisher+', '+date+'</p>\
+            <p class="container d-flex paper-publisher "><b>'+publisher+', '+date+'</b></p>\
         </div>\
         <div class="container w-25 ">\
             <div class="container d-flex justify-content-end">\
+                <a href='+link+'>\
                 <i class="fa-solid fa-file fa-xl icon"></i>\
+                </a>\
             </div>\
         </div>\
     </div>'
